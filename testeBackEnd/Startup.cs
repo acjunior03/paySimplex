cc43b69976bcd942dbb42857508cc8ebf93d67f2
Antonio Carlos Pinheiro de Lima Junior <acjunior04@hotmail.com>
2020-12-04 11:24:32 -0300

    fim

--- testeBackEnd/Startup.cs
+++ testeBackEnd/Startup.cs
@@ -1,26 +1,17 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using PaySimplex.Dados.Modelos;
 using PaySimplex.Dados.Controle;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerUI;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace testeBackEnd
 {
